71312e62f01bba84cbdaced489da27dc5ce1d587
v7ja <142932805+v7ja@users.noreply.github.com>
2024-02-01 20:44:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,80 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# ---------------------
-# Telegram : @DIBIBl , @TDTDI ,@ruks3
-# Coded by ruks
-# YouTube : https://youtube.com/channel/UCUNbzQRjfAXGCKI1LY72DTA
-# Instagram : https://instagram.com/_v_go?utm_medium=copy_link
-# ---------------------
-import requests,time,random,threading
-from user_agent import generate_user_agent
-Number = 0
-Hlist = []
-def send_uesr(uesr):
-	tt=time.asctime()
-	req = requests.post(f'https://api.telegram.org/bot{tok}/sendMessage?chat_id={id}&text=⌯  ʜɪ ѕɪʀ  ⌯\n. — — — — —  — — — — — . \n⌯ ᴜѕᴇʀɴᴀᴍᴇ : @{uesr}\n⌯ {tt} \n. — — — — —  — — — — —\n• Tele : @Prsey .')
-def check_uesr_xbox_ruks():
-	global Number
-	while True:
-		Number +=1
-		ruks = str("".join(random.choice( 'poiuytrewqasdfghjklmnbvcxz1234567890')for i in range(count)))	
-		req_ruks=requests.get(f"https://xboxgamertag.com/search/{ruks}",headers={'Host': 'xboxgamertag.com','cache-control': 'max-age=0','sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"','sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"','save-data': 'on','upgrade-insecure-requests': '1','user-agent': generate_user_agent(),'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','sec-fetch-site': 'none','sec-fetch-mode': 'navigate','sec-fetch-user': '?1','sec-fetch-dest': 'document'}).status_code	
-		if req_ruks == 200:
-			print(jruks+'['+BRed+f'{Number}'+jruks+'] Unavailable'+BRed+f'-[{ruks}]')
-		elif req_ruks == 404:
-			print(jruks+'['+BGreen+f'{Number}'+jruks+'] Available'+BGreen+f' [{ruks}]')
-			send_uesr(ruks)
-		else: pass	
-		
-ruks_ = '\033[1;36m'	
-ruks__ = '\033[1;36m'
-jruks = '\033[1;33m'
-_ruks  = '\033[1;31m'
-BGreen='\033[1;32m'
-BRed ='\033[1;31m'
-T=("="*60)
-print(f"""{BRed}{T}
-{BGreen}
-    ___  _   _  ____  ___  _  _    _  _  ____  _____  _  _   
-   / __)( )_( )( ___)/ __)( )/ )  ( \/ )(  _ \(  _  )( \/ )  
-  ( (__  ) _ (  )__)( (__  )  (    )  (  ) _ < )(_)(  )  (   
-   \___)(_) (_)(____)\___)(_)\_)  (_/\_)(____/(_____)(_/\_)  
-{jruks}
-
-	        __          __       __             
-	 ____  / /  __ __  / /___ __/ /__ ___   ____
-	/___/ / _ \/ // / / __/ // /  '_/(_-<  /___/
-	     /_.__/\_, /  \__/\_,_/_/\_\/___/       
-	          /___/                             
-	
-{BRed}{T}""")
-tok = input(jruks+'['+_ruks+'+'+jruks+']'+ruks__+' TOKEN BOT ! -> ; '+BGreen)
-id = input(jruks+'['+_ruks+'+'+jruks+']'+ruks__+' ID ! -> ; '+BGreen)
+from telethon import TelegramClient, sync, errors
+from telethon.sessions import StringSession
+from telethon.tl.functions.account import CheckUsernameRequest, UpdateUsernameRequest
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import SendMessageRequest
+from datetime import datetime
+import random, time, requests, telebot , os
+R = '\033[1;31m'
+y = '\033[1;33m'
+G= "\033[1;92m"
+chat_id = input('- Enter Id : ')
+token = input('- Enter Token : ')
 try:
-	count = int(input(jruks+'['+_ruks+'+'+jruks+']'+ruks__+' Character count ! -> ; '+BRed))
+	bot = telebot.TeleBot(token)
 except:
-	print("="*30)
-	print("Please put numbers only")
-	exit(0)
-if count >10: print("="*30),print("Please write a number less than 10"),exit()
+	exit('- Trun On Vpn / Error Token Bot')
+def check(client, username):
+    global bot
+    global chat_id
+    requ = requests.get("https://fragment.com/username/" + username)
+    if '<span class="tm-section-header-status tm-status-avail">Available</span>' in requ.text:
+        print("UserName Available In Fragment : " + username)
+        return "sale"
+    time.sleep(2)
+    try:
+        result = client(CheckUsernameRequest(username=username))
+        if result:
+            print(G+"UserName Available : " + username)
+            bot.send_message(chat_id=chat_id,text=username)
+        else:
+            print(R+"UserName Not Available : " + username)
+    except errors.FloodWaitError as timb:
+        print(f'You Have Been Blocked Wait {timb.seconds}')
+        time.sleep(timb.seconds)
+    except errors.UsernameInvalidError:
+        print(R+"Username Invalid : " + username)
+    except errors.rpcbaseerrors.BadRequestError:
+        print(R+"Username Banned : " + username)
 
-if __name__ == '__main__':
-	print("="*60)	
-	for i in range(5):
-		H1 =threading.Thread(target=check_uesr_xbox_ruks)
-		H1.start()
-		Hlist.append(H1)
-	for H2 in Hlist:
-		H2.join								
-		
-
-
-
-				
-								
-		
+def username(client):
+    AB = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    num = '1234567890'
+    while True:
+        mm = str("".join(random.choice(AB) for i in range(1)))
+        nn = str("".join(random.choice(AB) for i in range(1)))
+        ww = str("".join(random.choice(num) for i in range(1)))
+        hh = str("".join(random.choice(AB) for i in range(1)))
+        c = (mm + mm + mm + "_" + nn )
+        c1 = (mm + "_" + nn + "_" + hh)
+        c2 = (mm + "_" + mm + mm + ww)
+        c3 = (mm + mm + mm + "_" + hh)
+        c4 = (hh + "_" + mm + mm + mm)
+        c5 = (nn  + mm + mm + "_" + mm)
+        c6 = (mm + "_" + nn + nn + nn)
+        c7 = (hh  + mm + ww + ww + ww)
+        c8 = (mm + hh + ww + ww + ww)
+        c9 = (mm + mm + ww + nn + nn)
+        user = (c,c1,c2,c3,c4,c5,c6,c7,c8,c9)
+        username = str("".join(random.choice(user)))
+        check(client, username)
+api_id = input('- Enter Api_Id : ')
+api_hash = input('- Enter Api_Hash : ')
+def session1():
+    client = TelegramClient(StringSession(), api_id, api_hash)
+    client.start()
+    session = client.session.save()
+    client.disconnect()
+    return session
+def main():
+    session = session1()
+    client = TelegramClient(StringSession(session), api_id, api_hash)
+    try:
+    	client.start()
+    except:
+    	exit('- Error Api_Id , Api_Hash')
+    client(JoinChannelRequest('@yabhiraq'))
+    os.system('clear')
+    username(client)
+    client.disconnect()
+main()
